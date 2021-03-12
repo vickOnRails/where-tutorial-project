8d2c01bd59c2be697454d384f7869f270ff8ca35
vickonrails <victorofoegbu0009@gmail.com>
2021-03-12 11:01:37 +0100

    refactor: change project structure

--- pages/countries/[country]/[states]/lgas.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const lgas = () => {
-  return <div>LGAs</div>;
-};
-
-export default lgas;
--- pages/countries/[country]/states.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const states = () => {
-  return <div>States</div>;
-};
-
-export default states;
